960351d1a0eb978d240f9b7b8f8bfeecbb533615
danieljardine04 <danogaryjardine@gmail.com>
2021-12-03 13:01:22 -0700

    Switched the ordered list Element from the HTML to an object in the JavaScript page.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,25 @@
 var headerEl = document.getElementById("title");
-var pageContentEl = document.getElementById("textContent");
+var pageContentEl = document.querySelector(".pageContent");
+var divEl = document.querySelector("#textContent");
 var paragraphEl = document.createElement("p");
-var scoresEl = document.getElementById("scores");
 var listEl = document.createElement("li");
 var keyboardEl = document.getElementById("keyboard");
 var timerEl = document.getElementById("timer");
+var orderedEl = document.createElement("ol");
+
 
 
 
 pageContentEl.appendChild(paragraphEl);
-scoresEl.appendChild(listEl);
+orderedEl.appendChild(listEl);
 
 var clearPage = function(){
     
 }
+
+var startUp = function(){
+    headerEl.textContent = "Speed Typer";
+    divEl.textContent = "Welcome to Speed Typer. Are you looking for a job where you have to sit in front of a computer all day? Great news! This is the place for you. Here at Speed Typer you can hone your typing skills to feel comfortable in any job you need to type at. We want to help YOU become a better typer. "
+}
+
+startUp();
