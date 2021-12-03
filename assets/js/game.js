801f4ea4cecbe3928a5e6ac20edd7f9cef6d8907
danieljardine04 <danogaryjardine@gmail.com>
2021-12-02 20:52:13 -0700

    Set up basic javaScript and HTML

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+var headerEl = document.getElementById("title");
+var pageContentEl = document.getElementById("textContent");
+var paragraphEl = document.createElement("p");
+var scoresEl = document.getElementById("scores");
+var listEl = document.createElement("li");
+var keyboardEl = document.getElementById("keyboard");
+var timerEl = document.getElementById("timer");
+
+
+
+pageContentEl.appendChild(paragraphEl);
+scoresEl.appendChild(listEl);
+
+var clearPage = function(){
+    
+}
