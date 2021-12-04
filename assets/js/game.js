b6eb2861e06a1424054060f37b79e21e96416d46
danieljardine04 <danogaryjardine@gmail.com>
2021-12-03 17:40:30 -0700

    Started the landing page with text 2 dropdowns and a start button

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,27 @@ var listEl = document.createElement("li");
 var keyboardEl = document.getElementById("keyboard");
 var timerEl = document.getElementById("timer");
 var orderedEl = document.createElement("ol");
+var containerEl = document.querySelector(".container");
+var formEl = document.createElement("form");
 
 
+var difficulty = [
+    {name: "Easy"},
+    {name: "Medium"},
+    {name: "Hard"},
+    {name: "Programming"}
+];
+
+var timeSelect = [
+    {time: "1 minute"},
+    {time: "2 minutes"},
+    {time: "5 minutes"},
+    {time: "10 minutes"}
+
+]
 
 
-pageContentEl.appendChild(paragraphEl);
+divEl.appendChild(paragraphEl);
 orderedEl.appendChild(listEl);
 
 var clearPage = function(){
@@ -18,8 +34,49 @@ var clearPage = function(){
 }
 
 var startUp = function(){
+    var hEl = document.getElementById("head");
+    var button = document.createElement("button");
+    var select = document.createElement("select");
+    var selectEl = document.createElement("select");
+    select.innerHTML = "<select name='time-option' class='select-dropdown'></select>";
+    selectEl.innerHTML= "<select name='mode-option' class='select-dropdown'></select>"
+    
+    containerEl.appendChild(formEl);
+    formEl.appendChild(select);
+    formEl.appendChild(selectEl);
+    formEl.appendChild(button);
+    
+    for(var i = 0; i < timeSelect.length; i++){
+        var optionEl = document.createElement("option");
+        select.appendChild(optionEl);
+        optionEl.value = timeSelect[i].time;
+        optionEl.textContent = timeSelect[i].time;
+    
+    }
+
+    for(var i = 0; i < difficulty.length; i++){
+        var option = document.createElement("option");
+        selectEl.appendChild(option);
+        option.value = difficulty[i].name;
+        option.textContent = difficulty[i].name;
+
+    }
+    
+    
+
+    
+    
+    
     headerEl.textContent = "Speed Typer";
-    divEl.textContent = "Welcome to Speed Typer. Are you looking for a job where you have to sit in front of a computer all day? Great news! This is the place for you. Here at Speed Typer you can hone your typing skills to feel comfortable in any job you need to type at. We want to help YOU become a better typer. "
+    hEl.textContent = "Our Purpose";
+    paragraphEl.textContent = "Welcome to Speed Typer. Are you looking for a job where you have to sit in front of a computer all day? Great news! This is the place for you. Here at Speed Typer you can hone your typing skills to feel comfortable in any job you need to type at. We want to help YOU become a better typer. "
+    button.textContent = "Start!"
+    console.log(hEl);
+}
+
+var processForm = function(time, mode){
+
+
 }
 
 startUp();
