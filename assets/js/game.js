dcc5ad923d269ba3be5106f56ec3d6fb26503bed
danieljardine04 <danogaryjardine@gmail.com>
2021-12-09 18:09:09 -0700

    Finished landing page with functionality working.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,17 @@
 var headerEl = document.getElementById("title");
 var pageContentEl = document.querySelector(".pageContent");
-var divEl = document.querySelector("#textContent");
+var divEl = document.querySelector(".textContent");
 var paragraphEl = document.createElement("p");
 var listEl = document.createElement("li");
 var keyboardEl = document.getElementById("keyboard");
 var hEl = document.getElementById("head");
 var timerEl = document.getElementById("timer");
 var orderedEl = document.createElement("ol");
-var containerEl = document.querySelector(".container");
+var containerEl = document.querySelector(".form-div");
 var formDiv = document.createElement("div");
-var select = document.createElement("select");
-var selectEl = document.createElement("select");
 var clockTime = 0
 
-var random_words_api = 'https://random-word-api.herokuapp.com/word?number=300'
+var random_words_api = 'https://random-word-api.herokuapp.com/word?number=300';
 
 
 divEl.appendChild(paragraphEl);
@@ -49,8 +47,6 @@ type: [
 
 
 var clearPage = function(){
-    select.innerHTML = "";
-    selectEl.innerHTML = "";
     hEl.textContent = "";
     paragraphEl.textContent = "";
     
@@ -61,75 +57,80 @@ var startUp = function(){
     var button = document.createElement("button");
     var div1 = document.createElement("div");
     var div2 = document.createElement("div");
+    var selectTimeEl = document.createElement("select");
+    var selectTypeEl = document.createElement("select");
     
-    select.innerHTML = "<select name='time' class='select-dropdown' id='time-option'></select>"
-    selectEl.innerHTML= "<select name='type' class='select-dropdown' id = 'type-option'></select>"
+   selectTimeEl.name = "time";
+   selectTypeEl.name= "type";
+   selectTimeEl.className = "dropdown";
+   selectTypeEl.className = "dropdown";
     
-    formDiv.append(div1, div2);
-    div1.appendChild(select);
-    div1.appendChild(selectEl);
+    divEl.append(div1, div2);
+    div1.appendChild(selectTimeEl);
+    div1.appendChild(selectTypeEl);
     div2.appendChild(button);
     
     for(i = 0; i < dropDown.time.length; i++){
         var option = document.createElement("option");
-        select.appendChild(option);
+        selectTimeEl.appendChild(option);
         option.value = dropDown.time[i].name;
-      option.textContent = dropDown.time[i].name; 
+        option.textContent = dropDown.time[i].name; 
       
     }
 
     for(i=0; i < dropDown.type.length; i++){
         var optionEl = document.createElement("option");
-        selectEl.appendChild(optionEl);
+        selectTypeEl.appendChild(optionEl);
         optionEl.textContent = dropDown.type[i].name; 
         optionEl.value = dropDown.type[i].name;
     }
-
-    var chosenTime = select.querySelector("select[name='time']");
-    var chosenType = selectEl.querySelector("select[name='type']");
     
-    formDiv.appendChild(button); 
+    
+    
     headerEl.textContent = "Speed Typer";
     hEl.textContent = "Our Purpose";
     paragraphEl.textContent = "Welcome to Speed Typer. Are you looking for a job where you have to sit in front of a computer all day? Great news! This is the place for you. Here at Speed Typer you can hone your typing skills to feel comfortable in any job you need to type at. We want to help YOU become a better typer. "
     button.textContent = "Start!"
     
-    button.addEventListener("click", buttonListener(chosenTime, chosenType));
-        
-    }
-
-    var buttonListener = function(time, type){
-        return function(){
-            processForm(time, type);
-        };
-    }
+    button.addEventListener("click", buttonListener);
     
+}
+
+var buttonListener = function(){
+    var chosenTime = document.querySelector("select[name='time']");
+    var chosenType = document.querySelector("select[name='type']");
+    processForm(chosenTime.value, chosenType.value);
+}
+
 
 
 
 var processForm = function(time, type){
     
-    var pickedTime = dropDown.time.clockNumber;
-    var pickedType = dropDown.type.name;
-    if(time === pickedTime){
-        clockTime = time * 60;
-    }
-
-    if(type === pickedType){
-        processType(type);
-    }
+    clockTime = time * 60;
+    processType(type);
+    
     console.log(time);
     console.log(type);
-    
+    }
+
+var processType = function(type){
+    switch(type){
+        case "Easy": {
+            clearPage();
+            type = random_words_api;
+            fetch(type).then(function(response){
+                response.json().then(function(value){
+                    typingPage(value)
+                });
+            })
+        }
+    }
 
 }
 
-var selectOptions = function(selected){
-    
-    if(dropDown.name === selected){
-        window.alert(selected);
-
-    }
+var typingPage = function(words){
+    console.log(words);
 }
 
 startUp();
